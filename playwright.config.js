c1f94c8301943017a97829f7ed4e0597dda7ee4d
RomanHarmatii <romaharmatii01@gmail.com>
2026-01-29 21:05:48 +0200

    Artifacts

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "allure-playwright",
+  reporter: "line",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     actionTimeout: 10 * 1000,
